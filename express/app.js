13477a3a10484326a697c180902017d4e61aba48
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-29 11:04:34 +0530

    router

--- express/app.js
+++ express/app.js
@@ -1,29 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require("./routes/admin");
 
-app.use("/", (req, res, next) => {
-  console.log("Always runs");
-  next();
-});
+const shopRoutes = require("./routes/shop");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send('<form action="/product" method="POST"> <input type="text" name="title">  <button type="submit">Submit</button> </form>');
-});
-
-app.post('/product',(req,res)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req, res, next) => {
-  console.log("In the another middle ware");
-  res.send("<h1> Express </h1>")
-});
-
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
